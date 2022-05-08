ebf66a13fba6399659fc892e6602110746dd598c
Klizlo <klizlo0111@gmail.com>
2022-05-08 19:08:06 +0200

    Add new operations for the user
    User can update its email.
    User can cancel the lecture reservation.

--- src/main/java/com/example/siirestapi/service/UserService.java
+++ src/main/java/com/example/siirestapi/service/UserService.java
@@ -2,32 +2,55 @@ package com.example.siirestapi.service;
 
 import com.example.siirestapi.model.Lecture;
 import com.example.siirestapi.model.User;
+import com.example.siirestapi.repository.LectureRepository;
 import com.example.siirestapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final LectureRepository lectureRepository;
 
     public List<User> getAllUsers(){
         return userRepository.findAll();
     }
 
-
-    public Set<Lecture> getUserLectures(String login) {
+    public ResponseEntity<?> getUserLectures(String login) {
         User user = userRepository.findByLogin(login);
         if (user == null){
-            return new HashSet<>();
+            return ResponseEntity.badRequest().body("Użytkownik o takim loginie nie istnieje");
         }
-        return user.getLectures();
+        return ResponseEntity.ok().body(user.getLectures());
     }
 
+    @Transactional
+    public ResponseEntity<String> changeEmail(String login, User user){
+        User userEdited = userRepository.findByLogin(login);
+        if(userEdited == null){
+            return ResponseEntity.badRequest().body("Użytkownik o podanym loginie nie istnieje");
+        }
+        userEdited.setEmail(user.getEmail());
+        return ResponseEntity.ok().body("Poprawnie zmieniono adres email");
+    }
+
+    @Transactional
+    public ResponseEntity<String> cancelBooking(String login, Long id) {
+        User editedUser = userRepository.findByLogin(login);
+        if (editedUser == null){
+            return ResponseEntity.badRequest().body("Użytkownik o podanym loginie nie istnieje");
+        }
+        Lecture editedLecture = lectureRepository.findById(id).orElse(null);
+        if (editedLecture == null){
+            return ResponseEntity.badRequest().body("Nie istnieje taka prelekcja");
+        }
+        editedUser.removeLectureToUser(editedLecture);
+        return ResponseEntity.ok().body("Pomyślnie zostałeś wypisany z prelekcji");
+    }
 }
